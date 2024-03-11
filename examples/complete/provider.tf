55e53bb583529f6d279d79e6f8e69f5b5d7bb620
kousik25 <kousikdittakavi1@gmail.com>
2024-03-11 15:01:37 +0530

    aws-vpc

--- examples/complete/provider.tf
+++ examples/complete/provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "daws76s-remote-state"
+    bucket = "devoops-remote-state"
     key    = "vpc-test"
     region = "us-east-1"
-    dynamodb_table = "daws76s-locking"
+    dynamodb_table = "devops-locking"
   }
 }
 
